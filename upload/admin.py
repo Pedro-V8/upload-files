dd6914fcdf0863fdb4cbcffda473064ed5c20ab0
Pedro Henrique de Deus Vieira <pedrophdv@outlook.com>
2022-09-02 08:07:11 -0300

    Alterações na estrutura dos Apps Django

--- upload/admin.py
+++ upload/admin.py
@@ -1,10 +1,39 @@
 from django.contrib import admin
-from .models import Document
+from .models import Document , Transacao
+
 # Register your models here.
 
 @admin.register(Document)
 class AdminDocuments(admin.ModelAdmin):
     fields = ['title' , 'uploadedFile' , 'dateTimeOfUpload']
     list_display = ['id' ,'title' , 'uploadedFile' , 'dateTimeOfUpload']
+
+@admin.register(Transacao)
+class AdminTransacao(admin.ModelAdmin):
+    fields = [
+        'banco_origem',
+        'agencia_origem',
+        'conta_origem',
+        'banco_destino',
+        'agencia_destino',
+        'conta_destino' ,
+        'valor_transacao',
+        'data_hora',
+        'upload_id',
+        'created_at'
+    ]
+    list_display = [
+        'id',
+        'banco_origem',
+        'agencia_origem',
+        'conta_origem',
+        'banco_destino',
+        'agencia_destino',
+        'conta_destino',
+        'valor_transacao',
+        'data_hora',
+        'upload_id',
+        'created_at'
+    ]
 
 
--- upload/models.py
+++ upload/models.py
@@ -14,3 +14,19 @@ class Document(models.Model):
     class Meta():
         verbose_name = 'Document'
         verbose_name_plural = 'Documentos'
+
+class Transacao(models.Model):
+    banco_origem = models.CharField(max_length=200)
+    agencia_origem = models.CharField(max_length=200)
+    conta_origem = models.CharField(max_length=200)
+    banco_destino = models.CharField(max_length=200)
+    agencia_destino = models.CharField(max_length=200)
+    conta_destino = models.CharField(max_length=200)
+    valor_transacao = models.IntegerField()
+    data_hora = models.CharField(max_length=200)
+    upload_id = models.ForeignKey(Document , on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta():
+        verbose_name = 'Transacao'
+        verbose_name_plural = 'Transacoes'
